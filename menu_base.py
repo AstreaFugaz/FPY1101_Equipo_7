5d9ecf3c96e1c7efa17cabd9491e31858b36ef0a
Ignacia Astrea Pavla Padilla Parraguez <ign.padilla@duocuc.cl>
2025-06-22 09:59:40 -0400

    Added datos_nacchi and its calling to menu_base.py

--- menu_base.py
+++ menu_base.py
@@ -1,19 +1,22 @@
+def datos_nacchi():
+    print("Mi nombre es Ignacia Padilla y tengo 34 años.")
+
 # Menú base del programa
 while True:
-print("\n--- MENÚ PRINCIPAL ---")
-print("1. Función de integrante 1")
-print("2. Función de integrante 2")
-print("3. Función de integrante 3")
-print("0. Salir")
-op = input("Seleccione opción: ")
-if op == "0":
-print("Programa finalizado.")
-break
-elif op == "1":
-pass # Aquí se llamará a la función del integrante 1
-elif op == "2":
-pass # Aquí se llamará a la función del integrante 2
-elif op == "3":
-pass # Aquí se llamará a la función del integrante 3
-else:
-print(" Opción inválida.")
+    print("\n--- MENÚ PRINCIPAL ---")
+    print("1. Función de integrante 1")
+    print("2. Función de integrante 2")
+    print("3. Función de integrante 3")
+    print("0. Salir")
+    op = input("Seleccione opción: ")
+    if op == "0":
+        print("Programa finalizado.")
+        break
+    elif op == "1":
+        datos_nacchi()
+    elif op == "2":
+        pass # Aquí se llamará a la función del integrante 2
+    elif op == "3":
+        pass # Aquí se llamará a la función del integrante 3
+    else:
+        print("Opción inválida.")
